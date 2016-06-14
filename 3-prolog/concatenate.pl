95d65ea920634d52e897575769b2972a2d907b15
pietro909 <pietro.grandi.3d@gmail.com>
2016-06-14 17:45:55 +0200

    reverse list

--- 3-prolog/concatenate.pl
+++ 3-prolog/concatenate.pl
@@ -1,4 +1,7 @@
 concatenate([], List, List) .
-% concatenate([Head|[]], List, [Head|List]) .
-concatenate([Head1|Tail1], List, [Head1|Tail2]) :-
+concatenate([Head|Tail1], List, [Head|Tail2]) :-
     concatenate(Tail1, List, Tail2) .
+
+reverse_list([],[]) .
+reverse_list([Head|Tail], List) :-
+    concatenate(TailToDo, [Head], List), reverse_list(Tail, TailToDo) .
