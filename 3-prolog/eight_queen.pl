68aeeed6eb05b5916541cf2e2fe26fe7abf4f9f0
pietro909 <pietro.grandi.3d@gmail.com>
2016-06-15 15:55:51 +0200

    working as validator, not as generator -_-

--- 3-prolog/eight_queen.pl
+++ 3-prolog/eight_queen.pl
@@ -1,29 +1,32 @@
-/*eq([_,_,_,_,_,_,_,_,
-    _,_,_,_,_,_,_,_,
-    _,_,_,_,_,_,_,_,
-    _,_,_,_,_,_,_,_,
-    _,_,_,_,_,_,_,_,
-    _,_,_,_,_,_,_,_,
-    _,_,_,_,_,_,_,_,
-    _,_,_,_,_,_,_,_],
-    Solution) .
-eq(Board, Solution) .
-eq(*/
-
-get_rows_cols([], CurrentRows, CurrentCols, Rows, Cols) :-
-    Rows = CurrentRows, 
+rec_get_rows_cols([], CurrentRows, CurrentCols, Rows, Cols) :-
+    Rows = CurrentRows,
     Cols = CurrentCols .
-
-get_rows_cols([[Row, Col]|Tail], CurrentRows, CurrentCols, Rows, Cols) :-
-    %nth0(0, Head, Row) ,
-    %nth0(1, Head, Col) ,
+rec_get_rows_cols([[Row, Col]|Tail], CurrentRows, CurrentCols, Rows, Cols) :-
     append([Row], CurrentRows, ActualRows) ,
     append([Col], CurrentCols, ActualCols) ,
     get_rows_cols(Tail, ActualRows, ActualCols, Rows, Cols) .
+get_rows_cols(Items, Rows, Cols) :-
+    rec_get_rows_cols(Items, [], [], Rows, Cols) .
+
+rec_get_diagonals([], CurrentNWs, CurrentSEs, NWs, SEs) :-
+    NWs = CurrentNWs ,
+    SEs = CurrentSEs .
+rec_get_diagonals([[Row, Col]|Tail], CurrentNWs, CurrentSEs, NWs, SEs) :-
+    Sum is Col + Row ,
+    Sub is Col - Row ,
+    append([Sum], CurrentNWs, ActualNWs) ,
+    append([Sub], CurrentSEs, ActualSEs) ,
+    rec_get_diagonals(Tail, ActualNWs, ActualSEs, NWs, SEs) .
+get_diagonals(Items, NWs, SEs) :-
+    rec_get_diagonals(Items, [], [], NWs, SEs) .
 
 eq(Queens) :-
     Queens = [Q1,Q2,Q3,Q4,Q5,Q6,Q7,Q8] ,
-    get_rows_cols(Queens, [], [], Rows, Cols) ,
+    get_rows_cols(Queens, Rows, Cols) ,
     fd_domain(Rows, 1, 8) ,
-    fd_domain(Cols, 1, 8) .
-
+    fd_all_different(Rows) ,
+    fd_domain(Cols, 1, 8) ,
+    fd_all_different(Cols) ,
+    get_diagonals(Queens, NWs, SEs) ,
+    fd_all_different(NWs) ,
+    fd_all_different(SEs) .
