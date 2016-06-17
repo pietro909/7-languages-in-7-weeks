fc52ef595f0761b469985796462efbe47f37a2f2
pietro909 <pietro.grandi.3d@gmail.com>
2016-06-17 10:59:15 +0200

    do shopping

--- 5-erlang/two.erl
+++ 5-erlang/two.erl
@@ -3,3 +3,5 @@
 
 % [{erlang, "a functional language"}, {ruby, "an OO language"}]
 find(List, Keyword) -> [Value || {Key, Value} <- List, Key == Keyword] .
+
+doShopping(List) -> [ {Item, Qty * Price} || {Item, Price, Qty} <- List] .
