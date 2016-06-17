4e67cd18be0c98aa549c3ec0ddc5acd13130cd5b
pietro909 <pietro.grandi.3d@gmail.com>
2016-06-17 10:08:43 +0200

    count to ten...

--- 5-erlang/one.erl
+++ 5-erlang/one.erl
@@ -1,15 +1,25 @@
 -module(one) .
 -export([words/1]) .
+-export([countToTen/1]) .
 
 % empty string
 words([]) -> 0 ;
 % one space string
 words([Head|[]]) when Head == 32 -> 0 ;
 % one letter string
-words([Head|[]]) -> 1 ;
+words([_|[]]) -> 1 ;
 % space and non-empty string
 words([Head|Tail]) when Head == 32 -> words(Tail) ;
 % letter and non-empty string, second letter is space
-words([Head|[Head2|Tail]]) when Head2 == 32 -> 1 + words(Tail) ;
+words([_|[Head2|Tail]]) when Head2 == 32 -> 1 + words(Tail) ;
 % letter and non empty string
-words([Head|Tail]) -> words(Tail) .
+words([_|Tail]) -> words(Tail) .
+
+
+countToTen(N) when N == 10 -> N ;
+countToTen(N) when N > 10 ->
+    erlang:display(N) ,
+    countToTen(N - 1) ;
+countToTen(N) ->
+    erlang:display(N) ,
+    countToTen(N + 1) .
